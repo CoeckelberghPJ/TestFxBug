56de225d9a53b6c0d9ffa0f8f2e1ea017bb4ae3d
Pieter Jan Coeckelbergh <PieterJan.Coeckelbergh@Katoennatie.com>
2021-07-14 08:36:19 +0200

    Add project files.

---
+++ Domain/User.cs
@@ -0,0 +1,28 @@
+﻿namespace Domain
+{
+    public class User
+    {
+        public static User Create(string lastName, string firstName)
+        {
+            return new User
+            {
+                LastName = lastName,
+                FirstName = firstName
+            };
+        }
+
+        public User(string lastName, string firstName)
+        {
+            LastName = lastName;
+            FirstName = firstName;
+        }
+
+        public User()
+        {
+        }
+
+        public string LastName { get; private set; }
+
+        public string FirstName { get; private set; }
+    }
+}
---
+++ Tests/UserTests.cs
@@ -0,0 +1,34 @@
+﻿using Domain;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace Tests
+{
+    [TestClass]
+    public class UserTests
+    {
+        [DataTestMethod]
+        [DynamicData(nameof(GetParseUserDatas), DynamicDataSourceType.Method)]
+        public void ParseUserDatasTest(User user, string lastName, string firstName)
+        {
+            //Assert
+            user.FirstName.Should().Be(firstName);
+            user.LastName.Should().Be(lastName);
+        }
+
+        public static IEnumerable<object[]> GetParseUserDatas()
+        {
+            yield return new object[] {
+                User.Create("Doe", "John"),
+                "Doe",
+                "John"
+            };
+            yield return new object[] {
+                new User("Doe", "Jane"),
+                "Doe",
+                "Jane "
+            };
+        }
+    }
+}
